2097c8d1d532a3a63f417969f40b7f0ab19bd968
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2021-10-13 22:43:36 -0300

    cambio index

---
+++ resources/views/students.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="js/funciones.js"></script>
+    <title>Ejercicio formativo 1</title>
+</head>
+<body>
+<center>
+    <h1>BIENVENID@</h1>
+<br>
+<h2><a href="./students/">Ingresar a control de alumnos</a></h2>
+</center>
+</body>
+</html>
